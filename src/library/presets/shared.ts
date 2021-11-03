b57d0a58a63a0080ed1f74b2b777359d36cd6c01
Vitaly Turovsky <vital2580@icloud.com>
2021-11-03 06:05:27 +0300

    add check for test dir

--- src/library/presets/shared.ts
+++ src/library/presets/shared.ts
@@ -1,3 +1,4 @@
+import { existsSync } from 'fs'
 import { Octokit } from '@octokit/rest'
 import execa from 'execa'
 import { readPackageJsonFile } from 'typed-jsonfile'
@@ -13,7 +14,7 @@ export type InputData = {
 }
 
 export type OutputData = void | {
-    assets: { name: string; path: string }[]
+    assets: Array<{ name: string; path: string }>
 }
 
 export type PresetExports = { main: PresetMain }
@@ -22,7 +23,7 @@ export type PresetMain = (data: InputData) => Promise<OutputData>
 
 // I don't think it's safe
 /** pipes output */
-export const safeExeca = async (command: string, args: string | string[], options: execa.Options<string> = {}) => {
+export const safeExeca = async (command: string, args: string | string[], options: execa.Options = {}) => {
     await execa(command, Array.isArray(args) ? args : args.split(' '), {
         stdio: 'inherit',
         ...options,
@@ -45,6 +46,9 @@ export const runTestsIfAny = async () => {
         startGroup('pnpm test')
         await safeExeca('pnpm', 'test')
         endGroup()
+    } else {
+        // a check for case when tests are exist, but forgot to setup them (e.g. add test script)
+        if (existsSync('test')) throw new Error('Test dir exists, but not the script')
     }
     // TODO run eslint with no-op
 }
