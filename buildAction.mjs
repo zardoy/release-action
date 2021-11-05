94f73d27b53c68530c9918418f5dcf39c4e906ae
Vitaly Turovsky <vital2580@icloud.com>
2021-11-05 19:06:44 +0300

    [skip ci] remove unused action files

--- buildAction.mjs
+++
@@ -1,9 +0,0 @@
-import { build } from 'esbuild'
-
-await build({
-    bundle: true,
-    platform: 'node',
-    minify: true,
-    entryPoints: ['src/github-action/index.ts'],
-    outfile: 'build-action/index.js',
-})
