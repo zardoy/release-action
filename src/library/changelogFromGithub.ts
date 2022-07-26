9d242e761fefab8b7b8fa4eacb9d97b8d3a8c14a
Vitaly Turovsky <vital2580@icloud.com>
2022-07-27 02:12:15 +0300

    impl tests, they're done!

--- src/library/changelogFromGithub.ts
+++ src/library/changelogFromGithub.ts
@@ -1,57 +1,26 @@
-import { graphql } from '@octokit/graphql'
 import urlJoin from 'url-join'
-import { OctokitRepo, OctokitRepoWithUrl } from './types'
+import { queryRepositoryReleases } from './queryRepositoryReleases'
+import { OctokitRepoWithUrl } from './types'
 
 const RELEASES_LIMIT = 100
 
-export const queryRepositoryReleases = async (
-    repo: OctokitRepo,
-): Promise<{ totalCount: number; releases: Array<{ createdAt: number; name: string; tagName: string; description: string }> }> => {
-    const graphqlQuery = graphql.defaults({
-        headers: {
-            authorization: `token ${process.env.GITHUB_TOKEN}`,
-        },
-    })
-    const gql = String.raw
-    const data = await graphqlQuery<any>(
-        // eslint-disable-next-line unicorn/template-indent
-        gql`
-            query someRepos($owner: String!, $name: String!) {
-                repository(owner: $owner, name: $name) {
-                    releases(first: 100, orderBy: { field: CREATED_AT, direction: DESC }) {
-                        totalCount
-                        nodes {
-                            createdAt
-                            name
-                            tagName
-                            description
-                        }
-                    }
-                }
-            }
-        `,
-        {
-            owner: repo.owner,
-            name: repo.repo,
-        },
-    )
-    const { totalCount, nodes: releases } = data.repository.releases
-    return { totalCount, releases }
+interface Options {
+    // lowerHeading?: false,
 }
 
-export const extractChangelogFromGithub = async (repo: OctokitRepoWithUrl) => {
+export const extractChangelogFromGithub = async (repo: OctokitRepoWithUrl, _options: Options = {}) => {
     // eslint-disable-next-line arrow-body-style
     const replaceHashAndIssues = (input: string) => {
         return input
-            .replace(/ [\da-f]{40}\b/g, sha => `[\`${sha.slice(0, 7)}\`](${repo.url}/commit/${sha})`)
-            .replace(/#(d+)\b/g, issueOrPr => `[${issueOrPr}](${urlJoin(repo.url, 'issues', issueOrPr)})`)
+            .replace(/(?<= )[\da-f]{40}\b/g, sha => `[\`${sha.slice(0, 7)}\`](${repo.url}/commit/${sha})`)
+            .replace(/#(\d+)\b/g, (match, issueOrPrNum) => `[${match}](${repo.url}/issues/${issueOrPrNum}})`)
     }
 
     const { totalCount, releases } = await queryRepositoryReleases(repo)
     let markdown = ''
     for (const { createdAt, name, tagName, description } of releases) {
         const dateFormatted = new Date(createdAt).toISOString().split('T')[0]!
-        markdown += `## [${name}](${urlJoin(repo.url, 'releases/tag', tagName)}) - ${dateFormatted}\n${replaceHashAndIssues(description)}`
+        markdown += `\n## [${name}](${urlJoin(repo.url, 'releases/tag', tagName)}) - ${dateFormatted}\n${replaceHashAndIssues(description)}`
     }
 
     if (totalCount > RELEASES_LIMIT)
--- test/changelogFromGithub.test.ts
+++ test/changelogFromGithub.test.ts
@@ -1,9 +1,16 @@
-import * as changelogFromGithub from '../src/library/changelogFromGithub'
+import { extractChangelogFromGithub } from '../src/library/changelogFromGithub'
 import { OctokitRepoWithUrl } from '../src/library/types'
 
+let mockedReleases = {}
+
+vi.mock('../src/library/queryRepositoryReleases', () => ({
+    queryRepositoryReleases() {
+        return mockedReleases
+    },
+}))
+
 const mockReleasesOnce = (releases: { name: string; description: string }[]) => {
-    const spy = vi.spyOn(changelogFromGithub, 'queryRepositoryReleases')
-    spy.mockResolvedValueOnce({
+    mockedReleases = {
         totalCount: releases.length,
         releases: releases.map(({ name, description }) => ({
             createdAt: Date.now(),
@@ -11,7 +18,7 @@ const mockReleasesOnce = (releases: { name: string; description: string }[]) => 
             name,
             tagName: name,
         })),
-    })
+    }
 }
 
 const dummyRepo: OctokitRepoWithUrl = {
@@ -23,7 +30,7 @@ const dummyRepo: OctokitRepoWithUrl = {
 test('Generates advaned changelog', async () => {
     mockReleasesOnce([
         {
-            name: 'v0.0.1',
+            name: 'v0.0.2',
             description: `<!-- bump-type:patch -->
 ### Bug Fixes
 
@@ -40,6 +47,24 @@ test('Generates advaned changelog', async () => {
 `,
         },
     ])
-    const changelog = await changelogFromGithub.extractChangelogFromGithub(dummyRepo)
-    expect(changelog).toMatchInlineSnapshot()
+    const changelog = await extractChangelogFromGithub(dummyRepo)
+    expect(changelog).toMatchInlineSnapshot(`
+      {
+        "markdown": "
+      ## [v0.0.2](https://github.com/owner/test/releases/tag/v0.0.2) - 2022-07-26
+      <!-- bump-type:patch -->
+      ### Bug Fixes
+
+      - fix something cool ([#9](https://github.com/owner/test/issues/9}), [#10](https://github.com/owner/test/issues/10}))
+      - another fix [\`919b378\`](https://github.com/some/repo/commit/919b378a3d01a3ff7ab1952c9ab792b84e0234be)
+      - yes [\`919b378\`](https://github.com/owner/test/commit/919b378a3d01a3ff7ab1952c9ab792b84e0234be)
+      ## [v0.0.1](https://github.com/owner/test/releases/tag/v0.0.1) - 2022-07-26
+
+      ### New Features
+      - other lines ([#9](https://github.com/owner/test/issues/9}))
+      - ### Introduce new snippet constrain! Meet **otherLines**!
+      ",
+        "totalCount": 2,
+      }
+    `)
 })
